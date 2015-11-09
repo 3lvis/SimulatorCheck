3f93640780b66d6f2de59d553e85b754e449a66e
Elvis Nuñez <3lvis@users.noreply.github.com>
2015-11-09 16:50:09 +0100

    Update SimulatorCheck.swift

--- Source/SimulatorCheck.swift
+++ Source/SimulatorCheck.swift
@@ -1,9 +1,9 @@
-struct SimulatorCheck {
+public struct SimulatorCheck {
     /**
      Method to check wheter your code is running on the simulator or not.
      - returns: A Bool, `true` if you're on the simulator, `false` if you're not.
      */
-    static let isSimulator: Bool = {
+    public static let isSimulator: Bool = {
         var isSimulator = false
         #if arch(i386) || arch(x86_64)
             isSimulator = true
